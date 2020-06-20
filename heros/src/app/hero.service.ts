b84848687b7b71f63e78663cebd091ccb9b53d49
Geoffrey Ferril <geoffferril@texasmutual.com>
2020-06-20 15:43:56 -0500

    test deploy_v2

---
+++ heros/src/app/hero.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+  
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
